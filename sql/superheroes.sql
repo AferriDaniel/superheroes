fbe6267d81d5407fbbac5ef92e923815de18ebdd
root <root@elbambino.home>
2021-10-08 11:31:04 +0200

    fix script error

--- sql/superheroes.sql
+++ sql/superheroes.sql
@@ -2,6 +2,6 @@ CREATE TABLE superhores(
     id integer NOT NULL,
     name text NOT NULL,
     year_of_creation character(8) NOT NULL,
-    attributes text NOT NULL,
+    attributes text NOT NULL
     )
 
