c9a763d3a53a1ea863a6ce3e76f696b7ee4af2fc
root <root@elbambino.home>
2021-10-08 11:49:53 +0200

    Adding new attributes to superheroes table

--- sql/superheroes.sql
+++ sql/superheroes.sql
@@ -1,7 +1,9 @@
 CREATE TABLE superhores(
     id integer NOT NULL,
     name text NOT NULL,
-    year_of_creation character(8) NOT NULL,
-    attributes text NOT NULL,
-    )
+    alter_ego text NOT NULL,
+    first_appearence text NOT NULL,
+    Place_of_origin text NOT NULL
+    Abilities text NOT NULL
 
+    )
