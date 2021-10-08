c0e149bc2fae4ef0aed19e52c16f8062e99c1abe
root <root@elbambino.home>
2021-10-08 16:47:43 +0200

    typo error in sql script

--- sql/superheroes.sql
+++ sql/superheroes.sql
@@ -1,8 +1,8 @@
-CREATE TABLE superhores(
+CREATE TABLE superheroes(
     id integer NOT NULL,
     name text NOT NULL,
     alter_ego text NOT NULL,
     first_appearence text NOT NULL,
-    Place_of_origin text NOT NULL
+    Place_of_origin text NOT NULL,
     Abilities text NOT NULL
     )
