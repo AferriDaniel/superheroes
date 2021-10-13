2482b072be8c2ef4a8c737dc4e5529c9e5f42a61
root <root@elbambino.home>
2021-10-13 20:14:13 +0200

    Adding Black Panther and Black widow"

--- sql/superheroes.sql
+++ sql/superheroes.sql
@@ -11,6 +11,8 @@ CREATE TABLE superheroes(
     INSERT INTO superheroes (id,name,alter_ego,first_appearence,Place_of_origin,Abilities)
     VALUES ('00000002','Wolverine','James Howlett','Cameo appearance: The Incredible Hulk #180 (October 1974)','Cold Lake, Alberta, Canada','Enhanced strength, speed, stamina, durability, agility, dexterity, and reaction time');
     INSERT INTO superheroes (id,name,alter_ego,first_appearence,Place_of_origin,Abilities)
-    VALUES ('00000003','Black Panther','T'Challa','Fantastic Four #52 (July 1966)','Wakanda, Africa','The power to draw upon the knowledge, strength and every experience of every previous Black Panther');
+    VALUES ('00000003','Black Panther',"T'Challa",'Fantastic Four #52 (July 1966)','Wakanda, Africa','The power to draw upon the knowledge, strength and every experience of every previous Black Panther');
+    INSERT INTO superheroes (id,name,alter_ego,first_appearence,Place_of_origin,Abilities)
+    VALUES ('00000004','Black Widow','Natalia Alianovna Romanova','Tales of Suspense #52 (April 1964)','Russia','Expert spy, tactician');
     
     
